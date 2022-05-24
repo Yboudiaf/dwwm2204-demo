032212e9cabef3d9fbeebda4cc1e81eed6325da4
boudiaf <yacyac1313@outlook.fr>
2022-05-24 12:45:42 +0200

    Create Exercice 3.6 jeux de la fourchette.cs

---
+++ Exercice 3.6 jeux de la fourchette.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace jeux_de_la_fourchette
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+              Random rnd = new Random();
+            int numberUser;
+            int random = rnd.Next(0, 100);
+            int fork =0;
+            int test =1;
+
+
+          
+            Console.WriteLine("Saisisez un nombre entier entre 0 et 100");
+
+            do
+            {
+                numberUser = int .Parse(Console.ReadLine());
+                if(random < numberUser)
+                {
+                    fork = numberUser;
+                    test++;
+                    Console.WriteLine("le nombre rechercher est plus petit que "+fork);
+                }else if(random > numberUser)
+                {
+                    fork = numberUser;
+                    test++;
+                    Console.WriteLine("le nombre rechercher est plus grand que " + fork);
+                }
+                else
+                {
+                     Console.WriteLine("bravo vous avez trouvé en " + test + " essai");
+                }
+           
+            } while (numberUser != random);
+
+
+
+        }
+    }
+}
