72883fa9568f4c72ec6b1cb1ec58ec24de020791
yacine <yacine@>
2022-06-13 12:52:22 +0200

    exercices script

---
+++ Exercice 4.5 palindrom/Exercice 4.5 palindrom/Program.cs
@@ -0,0 +1,8 @@
+﻿int [] yacine = new int[10];
+
+for (int i = 0; i < 10; i++)
+{
+    yacine[i] = Convert.ToInt16(Console.ReadLine());
+    Console.WriteLine(yacine[i]);
+   // Array.Sort(yacine);
+} 
