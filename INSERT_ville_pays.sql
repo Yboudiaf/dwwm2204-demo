b8f80169552429597b04c636c921c587f53d30cc
yacine <yacine@>
2022-09-16 11:46:54 +0200

    requête  implémenter

--- INSERT_ville_pays.sql
+++
@@ -1,9 +0,0 @@
-
-	INSERT INTO cities(city_id,city_name)
-	VALUES (1,'Paris'),(2,'Lyon'),(3,'Toulouse'),(4,'Marseille'),(5,'Mulhouse'),(6,'Boston'),(7,'Los Angeles'),(8,'Washington'),(9,'New York'),(10,'Moscou'),(11,'Saint-Pétersbourg');
-	SELECT * FROM cities;
-	
-	
-	INSERT INTO countries(country_code,country_name)
-	VALUES ('FR','France'),('FR','France'),('FR','France'),('FR','France'),('FR','France'),('US','United States'),('US','United States'),('US','United States'),('US','United States'),('RU','Russian Federation'),('RU','Russian Federation');
-	
--- select_voitures.sql
+++
@@ -1,4 +0,0 @@
-select * from cars;
-select * from owners;
-select * from brands;
-select * from cars_owner;
