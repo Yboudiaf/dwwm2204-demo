a3a79c24271fb444734babb2eb280f3e9c48618f
yacine <yacine@>
2022-11-28 16:07:10 +0100

    gérer les formulaires js

--- formu_saisie/accueil.js
+++ formu_saisie/accueil.js
@@ -4,24 +4,26 @@
 function recupereCookie(nomCookie)
 { 
   // let decodedCookie = decodeURIComponent(document.cookie);
-    let currentCookie = document.cookie.split("; ");
-    console.log(currentCookie);
+    let tabcookies = document.cookie.split("; ");
+   // console.log(tabcookies);
     let trouver = false;
 
-    for (let index = 0; index < currentCookie.length; index++) {
+    for (let index = 0; index < tabcookies.length; index++)
+     {
 
-      let getCookie = currentCookie[index].split("=");
+      let getCookie = tabcookies[index].split("=");     
        // let c =  cookieDecoder[index];
        // while (c.charAt(0)==' '){
            // c = c.substring(1); 
-           if(getCookie[0] == nomCookie){
+           if(getCookie[0] == nomCookie)
+        {
 
             trouver = true;
             return getCookie[1];
 
         }
 
-        }
+    }
 
        if(trouver == false){
 
@@ -57,13 +59,74 @@ function recupereCookie(nomCookie)
 //   var labelCookie1 = document.createElement("label");
 //   labelCookie1.innerHTML = recupereCookie("dateAnniversaire") + "<br>";
 //   document.getElementById("modulo").appendChild(labelCookie1);
+ document.getElementById('nom').textContent += " "+recupereCookie('nom_utilisateur');
+ document.getElementById("prenom").textContent += " " +recupereCookie("prenom_utilisateur");
+ document.getElementById("age").textContent += " " +calculAge();
+ document.getElementById("anniversaire").textContent += " "+ recupereCookie("dateAnniversaire");
 
- document.querySelector('#modulo').innerHTML = recupereCookie("Pseudo");
- console.log(recupereCookie("Pseudo"));
 
- document.getElementById("dateAnniversaire").textContent = recupereCookie("dateAnniversaire");
- document.getElementById("nom_utilisateur").textContent = recupereCookie("nom_utilisateur");
+ document.getElementById("pseudo").textContent +=" "+ recupereCookie("Pseudo");
 
+var chaineDate = recupereCookie("dateAnniversaire");
+
+var tabDate = chaineDate.split("/");
+
+//////////////////////////////////////////
+//////////////////////////////////////////
+
+
+function calculNombreDeJour()
+{
+  let currentAnnee = (new Date()).getFullYear();
+ let dateAnniv = new Date(currentAnnee,Number(tabDate[1])-1,tabDate[0])
+
+ let dateJour = new Date();
+ if (dateJour > dateAnniv) {
+
+  dateAnniv = dateAnniv.setFullYear(currentAnnee +1);
+  let duree = (dateAnniv - dateJour)/(24*60*60*1000);
+  
+  return duree.toFixed(0);
+  
+ }else
+ {
+  let duree = (dateAnniv - dateJour)/(24*60*60*1000);
+  return duree.toFixed(0);
+ }
+
+ 
+
+}
+console.log(calculNombreDeJour());
+document.getElementById('jourAnniv').textContent += calculNombreDeJour();
+
+//////////////////////////////////////////
+//////////////////////////////////////////
+
+function calculAge()
+{
+    let anneeActuelle = new Date();
+
+    let annee = anneeActuelle.getFullYear();
+    console.log(annee);
+
+    let dateAnniv = recupereCookie("annee");
+    // console.log(document.getElementById('age'))
+    console.log(dateAnniv);
+
+    for (let index = annee; annee >= dateAnniv; index++)
+
+    {
+
+     let nombreAnnee = (annee - dateAnniv)
+
+    return nombreAnnee;
+       
+    }
+   
+
+}
+console.log(calculAge());
 
 
 
