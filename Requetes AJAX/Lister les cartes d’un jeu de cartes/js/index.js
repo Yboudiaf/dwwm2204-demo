4da535a09952c1417c98a7afe2efec62c23e2b1d
yacine <yacine@>
2022-11-30 16:10:40 +0100

    requête ajax

--- Requetes AJAX/Lister les cartes d’un jeu de cartes/js/index.js
+++ Requetes AJAX/Lister les cartes d’un jeu de cartes/js/index.js
@@ -7,39 +7,61 @@ monxhr.send();
 
 monxhr.onload = function ()
 {
-        if (monxhr.status==200) {
-
-            let container = monxhr.response;
-            createTable(container);
-          //  console.log(container);
-            
-        }
-        else
-        {
-            console.log("erreur : "+ monxhr.status + monxhr.statusText);
-        }
+  if (monxhr.status==200) {
+    let container = monxhr.response;
+    create_Table(container);
+    create_Legende();
+    console.log(container);
+  }
+  else
+  {
+    console.log("erreur : "+ monxhr.status + monxhr.statusText);
+  }
 }
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-function createCelule(maLigne , mesValeurs)
+function create_Cellule(maLigne , mesValeurs)
 {
-    let cellule = maLigne.insertCell();
-    cellule.textContent = mesValeurs;
-     
+  let cellule = maLigne.insertCell();
+  cellule.textContent = mesValeurs; 
 }
+function create_Title_Cellule(maLigne , mesValeurs)
+{
+  let cellule = document.createElement("th");
+  cellule.textContent = mesValeurs;
+  maLigne.append(cellule); 
+}
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+function create_Table(_data)
+{ 
+  let enTete = document.querySelector("#jeux");
+  let ligneTete = enTete.createTHead();
+  let ligneTitre = ligneTete.insertRow();
+  
+  for (const key in _data[0]) {    
+    create_Title_Cellule(ligneTitre , key );     
+  }
 
+  let myBody= enTete.createTBody();
 
-
- function createTable(_data)
- { 
-
-      let thead = document.querySelector("#jeux").createTHead();
+  for (let index = 0; index < _data.length; index++) {
+    const ligneBody = myBody.insertRow();
     
-       let ligneTitre = thead.insertRow();
-     for (const key in _data[0]) {
-        
-        createCelule(ligneTitre , key );     
+  for (const key in _data[1]) {    
+    createTBody(ligneTitre , key  );     
+  }
+  }
+}
+ ////////////////////////////////////////////////
+ ////////////////////////////////////////////////
+function create_Legende()
+{
+  let legende = document.querySelector("#jeux");
+  let addLegende = legende.createCaption();
+  addLegende.textContent = 'BDJ';
+}
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
 
-     }
-    
- }
+
